022bdaeab58ba9a60eb73a7ced37181edfa15f61
avanmalleghem <a.vanmalleghem@gmail.com>
2019-12-17 11:30:00 +0100

    Fix issue PackNuGetPackages script

--- Build/PackNuGetPackages.cmd
+++ Build/PackNuGetPackages.cmd
@@ -1,1 +1,5 @@
-call NuGet.exe pack "..\CM.Text\CM.Text.csproj" -Prop Configuration=Release -OutputDirectory "..\Output\Nuget" -IncludeReferencedProjects -build
+set BUILD_DIR=%~dp0
+set OUTPUT_DIR=%BUILD_DIR%..\Output\Nuget
+set PROJECT_FILE=%BUILD_DIR%..\CM.Text\CM.Text.csproj
+
+call %BUILD_DIR%NuGet.exe pack %PROJECT_FILE% -Prop Configuration=Release -OutputDirectory %OUTPUT_DIR% -IncludeReferencedProjects -build
